84cd7b2f30b669ed68666cbe627fe0e55da3956a
lucianobs1 <lucianotetsu@gmail.com>
2020-07-30 20:14:53 -0300

    Add layouts de introdução ao app

--- app/src/main/java/com/example/organizze/MainActivity.java
+++ app/src/main/java/com/example/organizze/MainActivity.java
@@ -4,11 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        CarregarSlides();
+    }
+
+    public void CarregarSlides(){
+
+        //Configurações de exibição de botoões
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+
+        this.addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorPrimary)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+
+        this.addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorPrimary)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+
+        this.addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorPrimary)
+                .fragment(R.layout.intro_3)
+                .build()
+        );
+
+        this.addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorPrimary)
+                .fragment(R.layout.intro_4)
+                .canGoBackward(true)
+                .canGoForward(false)
+                .build()
+        );
     }
 }
