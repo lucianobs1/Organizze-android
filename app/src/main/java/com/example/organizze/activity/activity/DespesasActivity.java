d1418ffbd4e4284a89fbcd015733c428ec60686e
lucianobs1 <lucianotetsu@gmail.com>
2020-08-08 14:28:11 -0300

    add logica para Pegar data atual

--- app/src/main/java/com/example/organizze/activity/activity/DespesasActivity.java
+++ app/src/main/java/com/example/organizze/activity/activity/DespesasActivity.java
@@ -3,14 +3,28 @@ package com.example.organizze.activity.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.organizze.R;
+import com.example.organizze.activity.helper.DateCustom;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class DespesasActivity extends AppCompatActivity {
+
+    private TextInputEditText campoData, campoCategoria, campoDescricao;
+    private EditText campoValor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_despesas);
+
+        campoValor = findViewById(R.id.editValor);
+        campoData = findViewById(R.id.editData);
+        campoCategoria = findViewById(R.id.editCategoria);
+        campoDescricao = findViewById(R.id.editDescricao);
+
+//        Preenche o campo data com o date atual
+        campoData.setText(DateCustom.dataAtual());
     }
 }
